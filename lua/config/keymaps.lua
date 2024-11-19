ac75793c0ab69dbe51bfa4e3cbfed4b8a4c629f1
jungai <juworaphol125@gmail.com>
2024-11-19 22:06:09 +0700

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,6 +7,9 @@ local map = LazyVim.safe_keymap_set
 map("n", "<leader>w_", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "<leader>w-", "<C-W>v", { desc = "Split Window Right", remap = true })
 
+-- diagnostics
+map("n", "<leader>D", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
+
 -- lazygit
 if vim.fn.executable("lazygit") == 1 then
   map("n", "<leader>lg", function()
